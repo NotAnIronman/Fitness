a06250429f27e89e6fb1ca79761b69135d91b54e
NotAnIronman <themonthofaugustus@gmail.com>
2026-08-03 22:04:18 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -24,7 +24,7 @@
    in sync with this).
    ============================================================ */
 
-const CACHE_VERSION = 'forge-v24';
+const CACHE_VERSION = 'forge-v25';
 
 const PRECACHE_URLS = [
   './',
